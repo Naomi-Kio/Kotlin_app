b6293601226dcde2b91624deb2217825e8199347
Наталья <hahulina.n.b@gmail.com>
2024-10-15 12:21:06 +0300

    test

---
+++ src/main2.kt
@@ -0,0 +1,24 @@
+//import java.util.*
+
+//fun main(args: Array<String>) {
+ //   val reader = Scanner(System.`in`)
+  //  print("Enter your name: ")
+  //  val name = reader.next()
+  //  print("Enter your age: ")
+  //  val age = reader.nextInt()
+  //  print("Enter your email: ")
+   // val email = reader.next()
+//}
+fun main() {
+    val data = run {
+        val day = readln()
+        val month = readln()
+        val year = readln()
+
+        "$day.$month.$year"
+    }
+
+    // Do not change the code below
+    println(data)
+}
+//test
